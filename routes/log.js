66ecee78aa1deb83e497105c2c07eb7bcc879819
hassam391 <hassamshahid351@yahoo.com>
2025-04-20 23:55:04 +0100

    Further bug fixing for log submission.

--- routes/log.js
+++ routes/log.js
@@ -1,28 +1,34 @@
-import express from "express";
-import Log from "../models/log.js";
-import authMiddleware from "../middleware/auth.js";
+const express = require("express");
+const Log = require("../models/log");
+const authMiddleware = require("../middleware/firebaseAuth");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, async (req, res) => {
    const { journeyType, data } = req.body;
    const userId = req.user.uid;
-   const username = req.user.username;
 
    const today = new Date().toISOString().split("T")[0];
 
    try {
+      const user = await User.findOne({ email });
+
+      //user validation
+      if (!user) {
+         return res.status(400).json({ message: "User not found" });
+      }
+
       //to avoid double logging
-      const existing = await Log.findOne({ userId, date: today });
+      const existing = await Log.findOne({ userId: user._id.toString(), date: today });
       if (existing) {
          return res.status(400).json({ message: "You've already logged today!" });
       }
 
       const newLog = new Log({
-         userId,
-         username,
-         date: today,
+         userId: user._id.toString(),
+         username: user.username,
          journeyType,
+         date: today,
          data,
       });
 
