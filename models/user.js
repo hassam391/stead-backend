3f10c5eb4a1d8f5debede5269d9ff918ec537252
hassam391 <hassamshahid351@yahoo.com>
2025-04-21 14:27:47 +0100

    Test commit for streak implementation for calories and tetsing.

--- models/user.js
+++ models/user.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 
 //defines user document in the database and its different attributes to send to mongodb
 const UserSchema = new mongoose.Schema({
+   username: { type: String, required: true, unique: true },
    email: { type: String, required: true, unique: true },
    journey: { type: String, default: null },
    frequency: { type: Number, default: 0 },
    goal: { type: String, default: null },
    activity: { type: String, default: null },
    calorieGoal: { type: Number, default: null },
-   username: { type: String, required: true, unique: true },
+   currentStreak: { type: Number, default: 0 }, //track the current streak
+   missedDays: { type: [Date], default: [] }, //array to store missed days
+   lastLoggedDate: { type: Date, default: Date.now }, //last day activity logged
 });
 
 module.exports = mongoose.model("User", UserSchema);
