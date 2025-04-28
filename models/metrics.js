6134ee06b68f9dbc91a04283a33be89c6f6b65fb
hassam391 <hassamshahid351@yahoo.com>
2025-04-28 15:12:25 +0100

    Added missing field to user.js. !6

--- models/metrics.js
+++ models/metrics.js
@@ -7,6 +7,7 @@ const MetricsSchema = new mongoose.Schema({
    streak: { type: Number, default: 0 }, //current streak
    lastLoggedDate: { type: Date, default: Date.now }, //last logged date
    missedDays: { type: [Date], default: [] }, //list of missed days
+   hasMetWeeklyLogTarget: false,
 });
 
 // Export the model
